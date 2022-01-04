9c429d2303fb96ef59e615fe039fac42eae00fbb
khalil <khalil94bouzayeni^gmail.com>
2022-01-04 10:48:04 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+const app = express()
+app.use(express.json())
+const mongoose = require ('mongoose')
+require('dotenv').config() 
+
+
+mongoose.connect(process.env.MONGO_URI,(err)=>err?console.log(err):console.log(`database connected`))
+
+
+app.use('/api/user',require('./routs/userRoute'))
+
+app.listen(process.env.PORT, (err)=>err?console.log(err):console.log(`server is runing`))
